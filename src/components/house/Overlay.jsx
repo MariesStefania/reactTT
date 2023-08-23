f14cff68e3d2fd06cd5c910e1e540e289def0823
MariesStefania <stefania_maries@yahoo.com>
2023-08-23 14:06:18 +0300

    changes

--- src/components/house/Overlay.jsx
+++ src/components/house/Overlay.jsx
@@ -4,17 +4,15 @@ import { useState } from "react";
 
 const Section = (props) => {
   return (
-    <section 
+    <section
       style={{
         opacity: props.opacity,
-        "margin":"300px",
+        margin: "300px",
       }}
     >
-      <div  style={{"margin-right":"800px"}}>
-        <div >
-          <div style={{"background":"#F7EFE6"}}>
-            {props.children}
-          </div>
+      <div style={{ "margin-right": "610px" }}>
+        <div>
+          <div style={{ background: "#F7EFE6" }}>{props.children}</div>
         </div>
       </div>
     </section>
@@ -23,19 +21,16 @@ const Section = (props) => {
 
 const Section2 = (props) => {
   return (
-    <section 
+    <section
       style={{
         opacity: props.opacity,
-        "margin":"300px",
-        "margin-top":"400px"
-
+        margin: "300px",
+        "margin-top": "400px",
       }}
     >
-      <div  style={{"margin-left":"800px"}}>
-        <div >
-          <div style={{"background":"#F7EFE6"}}>
-            {props.children}
-          </div>
+      <div style={{ "margin-left": "600px" }}>
+        <div>
+          <div style={{ background: "#F7EFE6" }}>{props.children}</div>
         </div>
       </div>
     </section>
@@ -44,19 +39,16 @@ const Section2 = (props) => {
 
 const Section3 = (props) => {
   return (
-    <section 
-    
+    <section
       style={{
         opacity: props.opacity,
-        "margin":"300px",
-        "margin-top":"550px"
+        margin: "300px",
+        "margin-top": "550px",
       }}
     >
-      <div style={{"margin-right":"800px"}}>
-        <div >
-          <div style={{"background":"#F7EFE6"}}>
-            {props.children}
-          </div>
+      <div style={{ "margin-right": "650px" }}>
+        <div>
+          <div style={{ background: "#F7EFE6" }}>{props.children}</div>
         </div>
       </div>
     </section>
@@ -77,56 +69,56 @@ export const Overlay = () => {
 
   return (
     <Scroll html>
-      <div >
-        <Section opacity={opacityFirstSection}  >
-          <h1  style={{"margin-left":"10px"}}>
-            O reprezentare a camerelor
-          </h1>
-          <p  style={{"margin-left":"10px","margin-right":"10px"}}>Model 3d realizat in blender si exportat in aplicatii web</p>
-          <p  style={{"margin-left":"10px"}}>camere:</p>
-          <ul >
+      <div>
+        <Section opacity={opacityFirstSection}>
+          <h1 style={{ "margin-left": "10px" }}>O reprezentare a camerelor</h1>
+          <p style={{ "margin-left": "10px", "margin-right": "10px" }}>
+            Model 3d realizat in blender si exportat in aplicatii web
+          </p>
+          <p style={{ "margin-left": "10px" }}>camere:</p>
+          <ul>
             <li>🧑‍💻 Camera de lucru</li>
             <li>🧑‍🏫 Camera de relaxare</li>
             <li>📦 Camera de idei</li>
           </ul>
-          <p  style={{"margin-left":"10px"}}>↓</p>
+          <p style={{ "margin-left": "10px" }}>↓</p>
         </Section>
         <Section2 right opacity={opacitySecondSection} mLeft={"800px"}>
-          <h1  style={{"margin-left":"10px"}}>
+          <h1 style={{ "margin-left": "10px" }}>
             De la calculator la o carte 🔥
           </h1>
-          <p  style={{"margin-left":"10px"}}>printr-un scroll se ajunge oriunde</p>
-          <p style={{"margin-left":"10px"}}>
+          <p style={{ "margin-left": "10px" }}>
+            printr-un scroll se ajunge oriunde
+          </p>
+          <p style={{ "margin-left": "10px" }}>
             <b>Interactiuni cu modelul 🔬</b>
           </p>
-          <ul style={{"margin-left":"10px"}}>
+          <ul style={{ "margin-left": "10px" }}>
             <li>Misca modelul stanga/dreapta</li>
             <li>Scroll sus/jos</li>
             <li>Misca modelul sus/jos</li>
           </ul>
-          <p  style={{"margin-left":"10px"}}>toate posibile datorita ThreeJs</p>
+          <p style={{ "margin-left": "10px" }}>
+            toate posibile datorita ThreeJs
+          </p>
 
-          <p  style={{"margin-left":"10px"}}>
+          <p style={{ "margin-left": "10px" }}>
             <b>Cum ?</b>
           </p>
-          <ul  style={{"margin-left":"10px"}}>
+          <ul style={{ "margin-left": "10px" }}>
             <li>Modelare 3d</li>
             <li>Imaginatie</li>
             <li>Libraria perfecta</li>
             <li>ThreeJS</li>
           </ul>
-          <p  style={{"margin-left":"10px"}}>↓</p>
+          <p style={{ "margin-left": "10px" }}>↓</p>
         </Section2>
         <Section3 opacity={opacityLastSection}>
-          <h1  style={{"margin-left":"10px"}}>
-            Cea mai buna librarie
-          </h1>
-          <p  style={{"margin-left":"10px"}}>
+          <h1 style={{ "margin-left": "10px" }}>Cea mai buna librarie</h1>
+          <p style={{ "margin-left": "10px" }}>
             De la modelare 3D la aplicatii web
           </p>
-          <p  style={{"margin-left":"10px"}}>
-          experiente imersive
-          </p>
+          <p style={{ "margin-left": "10px" }}>experiente imersive</p>
         </Section3>
       </div>
     </Scroll>
